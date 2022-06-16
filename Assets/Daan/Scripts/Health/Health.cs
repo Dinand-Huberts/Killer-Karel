7289d9b5c3d3bec9f1f82ee0f204d7a34106c746
Daan06 <daandeboer2@gmail.com>
2022-06-16 10:07:01 +0200

    HP

---
+++ Assets/Daan/Scripts/Health/Health.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+
+    [SerializeField] private float startingHealth;
+    public float currentHealth { get; private set; }
+    private Animator anim;
+    private bool dead;
+
+    private void Awake()
+    {
+        currentHealth = startingHealth;
+        anim = GetComponent<Animator>();
+    }
+
+    public void TakeDamage(float _damage)
+    {
+        currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
+        
+        if(currentHealth > 0)
+        {
+            anim.SetTrigger("hurt");
+        }
+        else
+        {
+            if(!dead)
+            {
+            // anim.SetTrigger("die");
+            // GetComponent<PlayerMovement>().enabled = false;
+            // GetComponent<ShootScript>().enabled = false;
+            // GetComponent<SwitchWeaponScript>().enabled = false;
+            Application.LoadLevel(Application.loadedLevel);
+            // dead = true;
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+       if(Input.GetKeyDown(KeyCode.F))
+       TakeDamage(1);
+    }
+
+    public void Die() 
+    {
+        Application.LoadLevel(Application.loadedLevel);
+    }
+}
