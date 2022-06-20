5a59558480dd54792fb7baf0949e14a7331b9bcf
inxype <73832073+inxype@users.noreply.github.com>
2022-06-20 12:53:02 +0200

    hotbar
    voor de hotbar

---
+++ Assets/sam/iventoryItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public interface iventoryItem
+{
+    string Name { get; }
+    
+    Sprite Image { get; }
+
+    void OnPickup();
+}
+
+public class InventoryEventArgs : EventArgs
+{
+    public InventoryEventArgs(IInventoryItem item)
+    {
+        Item = item
+    }
+
+    public IInventoryItem Item;
+}
