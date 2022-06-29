9d3c79b362ad35e031ffc8f2a996a6acbf1ee74a
inxype <73832073+inxype@users.noreply.github.com>
2022-06-29 08:43:04 +0200

    je moeder

---
+++ Assets/sam/slot.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class slot : MonoBehaviour
+{
+ public void DropItem(){
+    foreach (Transform child in transform)
+    {
+        
+    }
+ }
+}
